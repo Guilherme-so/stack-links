35893cab21fa83834e602bf6c88070bd999d9482
guilherme <guilhermemagno09@gmail.com>
2022-08-03 22:13:21 -0300

    adding typeDefs, resolvers, context and graphql server with apollo-server-micro

---
+++ pages/api/graphql.ts
@@ -0,0 +1,32 @@
+import Cors from 'micro-cors'
+import { ApolloServer } from 'apollo-server-micro'
+import { typeDefs } from '../../graphql/schema'
+import { resolvers } from '../../graphql/resolvers'
+import { createContext } from '../../graphql/context'
+
+const cors = Cors()
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: createContext,
+})
+
+const serverStart = apolloServer.start()
+
+export default cors(async function handler(req, res) {
+  if (req.method === 'OPTIONS') {
+    res.end()
+    return false
+  }
+  await serverStart
+  await apolloServer.createHandler({
+    path: '/api/graphql',
+  })(req, res)
+})
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
