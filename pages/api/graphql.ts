9024e3f4aa805b93ef64ebb29e5cfc15b47df8be
guilherme <guilhermemagno09@gmail.com>
2022-08-04 22:17:06 -0300

    adding apollo client

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,13 +1,13 @@
 import Cors from 'micro-cors'
 import { ApolloServer } from 'apollo-server-micro'
-import { typeDefs } from '../../graphql/schema'
+import { schema } from '../../graphql/schema'
 import { resolvers } from '../../graphql/resolvers'
 import { createContext } from '../../graphql/context'
 
 const cors = Cors()
 
 const apolloServer = new ApolloServer({
-  typeDefs,
+  schema,
   resolvers,
   context: createContext,
 })
